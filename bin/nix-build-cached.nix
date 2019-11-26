be1a5f54600d1436d1058be37050c77a6b3e7f5f
zimbatm <zimbatm@zimbatm.com>
2019-11-26 22:14:18 +0100

    nix-build-cached

---
+++ bin/nix-build-cached.nix
@@ -0,0 +1,28 @@
+let
+  src = <nix_file>;
+
+  # using scopedImport, replace readFile with
+  # implementations which will log files and paths they see.
+  #
+  # Note that scopedImport is not memoized, contrary to import
+  overrides = {
+    import = scopedImport overrides;
+    scopedImport = x: builtins.scopedImport (overrides // x);
+    builtins = builtins
+    // {
+      readFile = file: builtins.trace "evaluating file '${toString file}'" (builtins.readFile file);
+      # TODO: add readDir
+    }
+    ;
+  };
+
+  imported =
+    let
+      raw = overrides.scopedImport overrides src;
+    in
+      if (builtins.isFunction raw)
+      then raw {}
+      else raw;
+in
+  #imported
+overrides.scopedImport overrides src
