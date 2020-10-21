3fccee4fab20652210dd4d8fbfac93fb825e4d32
zimbatm <zimbatm@zimbatm.com>
2020-10-21 15:48:04 +0200

    nix-snapshot: .

---
+++ nix-snapshot/snapshot-in-drv.nix
@@ -0,0 +1,17 @@
+let
+  nixConfig = import <nix/config.nix>;
+  nixBin = builtins.storePath nixConfig.nixBinDir;
+in
+{ system ? builtins.currentSystem }:
+  derivation {
+    inherit system;
+    name = "snapshot-in-drv";
+    builder = "/bin/sh";
+    args = [ "-ec" ". $buildScriptPath" ];
+    passAsFile = [ "buildScript" ];
+    buildScript = ''
+      export HOME=$PWD/home
+      export NIX_PATH=nixpkgs=${<nixpkgs>}
+      ${nixBin}/nix-instantiate --store $PWD/nix ${<path>} > $out
+    '';
+  }
