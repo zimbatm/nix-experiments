ff71c42a6bd6e9d3b28d7e2c6ce0d39f748521cd
zimbatm <zimbatm@zimbatm.com>
2020-11-29 17:00:48 +0100

    .

--- pure-nix/toNix.nix
+++ pure-nix/toNix.nix
@@ -1,24 +1,12 @@
 let
-  inherit (builtins)
-    attrNames
-    concatStringsSep
-    isAttrs
-    isInt
-    isList
-    isPath
-    isString
-    replaceStrings
-    typeOf
-    ;
-
   # Copied from <nixpkgs/lib>
   mapAttrsToList = f: attrs:
-    map (name: f name attrs.${name}) (attrNames attrs);
+    map (name: f name attrs.${name}) (builtins.attrNames attrs);
 
   # Escape Nix strings
   stringEscape = str:
     "\"" + (
-      replaceStrings
+      builtins.replaceStrings
         [ "\\" "\"" "\n" "\r" "\t" ]
         [ "\\\\" "\\" "\\n" "\\r" "\\t" ]
         str
@@ -27,16 +15,16 @@ let
 
   # Like builtins.JSON but to output Nix code
   toNix = value:
-    if isString value then stringEscape value
-    else if isInt value then toString value
-    else if isPath value then toString value
+    if builtins.isString value then stringEscape value
+    else if builtins.isInt value then toString value
+    else if builtins.isPath value then toString value
     else if true == value then "true"
     else if false == value then "false"
     else if null == value then "null"
-    else if isAttrs value then
-      "{ " + concatStringsSep " " (mapAttrsToList (k: v: "${k} = ${toNix v};") value) + " }"
-    else if isList value then
-      "[ ${ concatStringsSep " " (map toNix value) } ]"
-    else throw "type ${typeOf value} not supported";
+    else if builtins.isAttrs value then
+      "{ " + builtins.concatStringsSep " " (mapAttrsToList (k: v: "${k} = ${toNix v};") value) + " }"
+    else if builtins.isList value then
+      "[ ${ builtins.concatStringsSep " " (map toNix value) } ]"
+    else throw "type ${builtins.typeOf value} not supported";
 in
 toNix
