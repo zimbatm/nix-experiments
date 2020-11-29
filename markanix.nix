8a0c25a11126b3a21e4673a8b24b5c938e07aa19
zimbatm <zimbatm@zimbatm.com>
2020-11-29 17:36:09 +0100

    markanix: renderHTML

--- markanix.nix
+++ markanix.nix
@@ -1,11 +1,10 @@
 # How would it look like if you could generate HTML5 with Nix?
 #
 # This is like https://rubygems.org/gems/markaby
-with builtins;
 rec {
   tag = type:
     let
-      elem = { _type = "tag"; type = type; };
+      elem = { tag = type; };
     in
     # either an attribute set or a null.
       # if null is passed, the 3rd argument here is ignored. Useful for <br/> or
@@ -16,39 +15,57 @@ rec {
     body:
     elem // {
       attrs = attrs;
-      body = if builtins.typeOf body == "string" then [ body ] else body;
+      body = if builtins.isString body then [ body ] else body;
     };
 
+  nulTag = type: attrs: tag type attrs null;
 
   # TODO: add <DOCTYPE> in front.
   html5 = tag "html";
   head = tag "head";
   body = tag "body";
-  meta = tag "meta";
+  meta = nulTag "meta";
   script = tag "script";
   div = tag "div";
-  hr = tag "hr";
+  hr = nulTag "hr";
   a = tag "a";
   # ... add all the tags here
 
-  # TODO:
-  renderHTML = { depth ? 0, elems }: "TODO";
+  # TODO: escape values if necessary
+  renderAttrs = attrs:
+    if attrs == { } then ""
+    else
+      " " +
+      (builtins.concatStringsSep " "
+        (map (k: "${k}=${toString attrs.${k}}") (builtins.attrNames attrs)));
+
+  renderHTML = tree:
+    if builtins.isAttrs tree then
+      "<${tree.tag}${renderAttrs tree.attrs}>"
+      +
+      (if tree.body == null then ""
+      else
+        (builtins.concatStringsSep "\n" (map renderHTML tree.body))
+        + "</${tree.tag}>")
+    else
+      toString tree
+  ;
 
   example =
     html5 { lang = "en"; } [
       (head { } [
-        (meta { lang = "utf-8"; } [ ])
+        (meta { lang = "utf-8"; })
         (script { src = "https://xxx"; } [ ])
       ])
       (body { } [
         (div { } [
           "Hey this is a text area"
-          (hr null)
+          (hr { })
           "And another"
           (a { href = https://example.com; } "Example website")
         ])
       ])
     ];
 
-
+  exampleRendered = renderHTML example;
 }
